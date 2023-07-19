91c2cb37734ebfae99aa777a3a8e90775c69cb79
kyun <fm13071303@rx.tnc.ne.jp>
2023-07-19 23:53:31 +0900

    top-worksまで制作

--- src/js/script.js
+++ src/js/script.js
@@ -30,4 +30,17 @@ jQuery(function ($) {
       delay: 3000,
     },
   });
+
+  // スクロールするとロゴの色変更
+  $(function () {
+    $(window).on("scroll", function () {
+      const sliderHeight = $(".mv").height();
+      if (sliderHeight - 30 < $(this).scrollTop()) {
+        $(".js-header").addClass("headerColorScroll");
+      } else {
+        $(".js-header").removeClass("headerColorScroll");
+      }
+    });
+  });
 });
+
